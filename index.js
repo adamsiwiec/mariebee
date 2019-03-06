42f5a82f8ec040b55f84a381813c7a522917b618
Adam Siwiec <adam.siwiec@ormlondon.com>
2019-03-06 10:26:38 -0600

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('I love you!'))
 
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}!`))
 
